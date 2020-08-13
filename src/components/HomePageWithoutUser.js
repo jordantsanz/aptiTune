489157de94447e2d34f3f60b84005acb5e1afa39
Sathvi <47374355+sathvi-k@users.noreply.github.com>
2020-08-13 19:06:19 -0400

    i am confusion

--- src/components/HomePageWithoutUser.js
+++ src/components/HomePageWithoutUser.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { helloWorld } from '../actions';
+
+class HomePageWithoutUser extends Component {
+  constructor(props) {
+    super(props);
+    console.log('mounting');
+  }
+
+  render() {
+    return (
+      <div> Hello world </div>
+    );
+  }
+}
+
+export default connect(null, { helloWorld })(HomePageWithoutUser);
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePageWithoutUser from './HomePageWithoutUser';
+import HomePageWithUser from './HomePageWithUser';
+
+const App = (props) => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePageWithoutUser} />
+        <Route path="/withuser" component={HomePageWithUser} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
