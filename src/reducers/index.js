559b6ad4b8d324258b6b854a07253f352d44cb51
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-08-14 18:44:01 -0500

    Hello woorld

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 // the starting point for your redux store
 // this defines what your store state will look like
+// need to fill this out
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
