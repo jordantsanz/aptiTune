9b6921f3cd2d04e5911334641b6a26bdd76088af
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-08-14 23:22:15 -0500

    Not much changed, just basic stuff

--- src/components/HomePageWithoutUser.js
+++ src/components/HomePageWithoutUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { helloWorld } from '../actions';
 
@@ -8,9 +9,24 @@ class HomePageWithoutUser extends Component {
     console.log('mounting');
   }
 
+  // authentication in this needs to be filled, links as well...but that is scary :(
   render() {
     return (
-      <div> Hello world </div>
+      <div className="background-homepage-without-user">
+        <div className="homepage-without-user-flex">
+          <div className="music-is-hard"> Music is hard. </div>
+          <div className="homepage-without-description"> We want to change that. <br />
+            Want to be the next taylor swift?
+            <br />
+            Let&apos;s make that happen.
+          </div>
+          <NavLink to="/newaccount" className="create-account">Create account</NavLink>
+          <input className="email" />
+          <input className="password" />
+          <div className="have-account">Already have an account? <NavLink to="/login" className="log-in">Log in here</NavLink></div>
+          <button type="submit" className="create-account">Create Account</button>
+        </div>
+      </div>
     );
   }
 }
