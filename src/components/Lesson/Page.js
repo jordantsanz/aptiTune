c2c191d897c50ce1dc919a0cc109b16c263f02d0
Jacob Donoghue <jacob.l.donoghue.22@dartmouth.edu>
2020-08-23 10:48:21 -0700

    with aws for audio

--- src/components/Lesson/Page.js
+++ src/components/Lesson/Page.js
@@ -6,6 +6,7 @@ import { NavLink, withRouter } from 'react-router-dom';
 import { getLesson } from '../../actions/index';
 import NavBar from '../NavBar';
 import FlatView from './Activities/FlatView';
+import Listening from './Activities/Listening';
 
 function mapStateToProps(reduxState) {
   return {
@@ -42,14 +43,15 @@ class Page extends Component {
       // console.log('this.props.pages.length', this.props.pages.length);
       if (this.props.pages.length > this.state.pageNumber + 1) {
         console.log('got inside');
-        const local = parseInt(this.state.nextPage, 10) + 1;
-        localStorage.setItem('setting next in gotoNext next', local.toString());
+        const local = parseInt(this.state.nextPage, 10);
+        localStorage.setItem('next', local.toString());
         this.setState((prevState) => ({ pageNumber: prevState.pageNumber + 1 }));
         this.setState((prevState) => ({ nextPage: prevState.nextPage + 1 }));
       } else {
+        // for now, redirect to home....
         const { history } = this.props;
         history.push('/home');
-        // for now, redirect to home....
+        // set user info to add
       }
     }
 
@@ -89,6 +91,43 @@ class Page extends Component {
               <div> Inserted flat api:</div>
               <div className="page-bottom">
                 <FlatView />
+                <button type="button" onClick={this.goToNext}>
+                  Next
+                </button>
+              </div>
+              {/*
+                <div className="activity-div">{page.act_type1}</div> {/* fill with this.props.page.activity
+                <div className="bottom-div">
+                  <NavLink to="/:username">
+                    <button className="button" id="next" type="button" onClick={this.getNextPage}>Next</button>
+                  </NavLink>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        );
+      } else if (page.activity_type === 'Listening') {
+        return (
+          <div className="current-page">
+            <NavBar />
+            <div className="full-page">
+              <div className="page-top">
+                <div className="page-top-topthird">
+                  <div className="page-top-title">{page.content.title}</div>
+                  <div className="page-top-nav">
+                    {/* Arrow icon from font awesome goes here pointing left */}
+                    <div className="page-top-nav-line" />
+                    <div className="page-top-nav-level">Level {this.state.pageNumber + 1} of {this.props.pages.length}</div>
+                    <div className="page-top-nav-line" />
+                    {/* Arrow icon from font awesome goes here pointing right */}
+                  </div>
+                </div>
+                <div className="page-top-description">{page.content.description}</div> {/* fill with this.props.page.description */}
+                <div className="page-top-content">{page.act_type1.instructions}</div>    {/* fill with this.props.page.content */}
+              </div>
+              <div> Inserted flat api:</div>
+              <div className="page-bottom">
+                <Listening />
                 <button type="button" onClick={this.goToNext}>
                   Next
                 </button>
