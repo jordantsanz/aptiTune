02ec9487807d99bfea8343844b134a262a3966df
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-08-24 14:36:54 -0500

    Scrolling and fade-in on signup page

--- src/components/HomePageWithoutUser.js
+++ src/components/HomePageWithoutUser.js
@@ -1,8 +1,17 @@
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable no-plusplus */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable eqeqeq */
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import VisbilitySensor from 'react-visibility-sensor';
+import { Spring } from 'react-spring/renderprops';
 import { signupUser } from '../actions/index';
+
+const empty = {
+};
 
 class HomePageWithoutUser extends Component {
   constructor(props) {
@@ -14,6 +23,8 @@ class HomePageWithoutUser extends Component {
       username: '',
       invalidInput: false,
     };
+    this.signupsection = React.createRef();
+    this.voicetomusic = React.createRef();
   }
 
   onInputEmail = (event) => {
@@ -62,6 +73,42 @@ class HomePageWithoutUser extends Component {
     }
   }
 
+  handleScrollSignUp = () => {
+    if (this.signupsection.current) {
+      this.signupsection.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }
+
+  handleScrollConvince = () => {
+    if (this.voicetomusic.current) {
+      this.voicetomusic.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }
+
+  fadeInScroll() {
+    document.on('scroll', () => {
+      const pageTop = document.scrollTop();
+      const pageBottom = pageTop + window.height();
+      const tags = '.tag';
+
+      for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+
+        if (tag.position().top < pageBottom) {
+          tag.addClass('visible');
+        } else {
+          tag.removeClass('visible');
+        }
+      }
+    });
+  }
+
   // fix link to sign-in page!!
   render() {
     return (
@@ -76,38 +123,114 @@ class HomePageWithoutUser extends Component {
               <NavLink to="/signin">
                 <button type="button" className="button" id="loginbutton">Login here</button>
               </NavLink>
-              <div className="subtitle" id="convinceme">Need to be convinced?</div>
+              <div className="subtitle" id="convinceme" onClick={this.handleScrollConvince}>Need to be convinced?</div>
             </div>
 
           </div>
+
           {/* absolutely no clue how to put the taylor swift words in, lol */}
+
           <div className="voice-to-music-section">
-            <div className="title" id="voice-to-music-title">Voice-to-music writing</div>
-            <div className="description" id="voice-to-music-description">AptiTune uses a <strong>live voice-to-sheet-music </strong>
-              translator. Sing a note into your computer&apos;s
-              microphone, and AptiTune writes the sheet music for you.
-            </div>
-            <div className="description" id="voice-to-music-bottom-description">
-              You learn by creating. <strong>It&apos;s that simple.</strong>
-            </div>
-            <button type="button" className="button" id="createAccountVoiceSection">Create Account</button> {/* need to implement autoscroll on these */}
+            <section id="convince">
+              <VisbilitySensor className="music-is-hard-section-invisible">
+                {({ isVisible }) => (
+                  <Spring delay={10} to={{ opacity: isVisible ? 1 : 0 }}>
+                    {({ opacity }) => (
+                      <div style={{ ...empty, opacity }} className="title" id="voice-to-music-title">Voice-to-music writing</div>
+                    )}
+                  </Spring>
+                )}
+              </VisbilitySensor>
+              <VisbilitySensor className="music-is-hard-section-invisible">
+                {({ isVisible }) => (
+                  <Spring delay={100} to={{ opacity: isVisible ? 1 : 0 }}>
+                    {({ opacity }) => (
+
+                      <div style={{ ...empty, opacity }} className="description" id="voice-to-music-description">AptiTune uses a <strong>live voice-to-sheet-music </strong>
+                        translator. Sing a note into your computer&apos;s
+                        microphone, and AptiTune writes the sheet music for you.
+                      </div>
+                    )}
+                  </Spring>
+                )}
+              </VisbilitySensor>
+              <VisbilitySensor className="music-is-hard-section-invisible">
+                {({ isVisible }) => (
+                  <Spring delay={100} to={{ opacity: isVisible ? 1 : 0 }}>
+                    {({ opacity }) => (
+
+                      <div style={{ ...empty, opacity }} className="description" id="voice-to-music-bottom-description">
+                        You learn by creating. <strong>It&apos;s that simple.</strong>
+                      </div>
+                    )}
+                  </Spring>
+                )}
+              </VisbilitySensor>
+              <VisbilitySensor className="music-is-hard-section-invisible">
+                {({ isVisible }) => (
+                  <Spring delay={100} to={{ opacity: isVisible ? 1 : 0 }}>
+                    {({ opacity }) => (
+
+                      <button ref={this.voicetomusic}
+                        type="button"
+                        className="button"
+                        id="createAccountVoiceSection"
+                        onClick={this.handleScrollSignUp}
+                        style={{ ...empty, opacity }}
+                      >Create Account
+                      </button>
+                    )}
+                  </Spring>
+                )}
+              </VisbilitySensor>
+
+            </section>
           </div>
+
           <div className="easy-section">
-            <div className="easy-title">Easy, accessible, and fun</div>
-            <div className="description" id="easy-description">We are students at Dartmouth who believe that learning
-              music <strong>should not</strong> be intimidating.
-            </div>
-            <div className="bottom-description">
-              There&apos;s so much material, and it can be daunting. It might feel like you need a natural talent that
-              you&apos;re born with...but let us prove you wrong. <br /> <strong>Learn music with us!</strong>
-            </div>
+            <VisbilitySensor className="music-is-hard-section-invisible">
+              {({ isVisible }) => (
+                <Spring delay={10} to={{ opacity: isVisible ? 1 : 0 }}>
+                  {({ opacity }) => (
+                    <div className="easy-title" style={{ ...empty, opacity }}>Easy, accessible, and fun</div>
+                  )}
+                </Spring>
+              )}
+            </VisbilitySensor>
+            <VisbilitySensor className="music-is-hard-section-invisible">
+              {({ isVisible }) => (
+                <Spring delay={100} to={{ opacity: isVisible ? 1 : 0 }}>
+                  {({ opacity }) => (
+                    <div className="description" style={{ ...empty, opacity }} id="easy-description">We are students at Dartmouth who believe that learning
+                      music <strong>should not</strong> be intimidating.
+                    </div>
+                  )}
+                </Spring>
+              )}
+            </VisbilitySensor>
+            <VisbilitySensor className="music-is-hard-section-invisible">
+              {({ isVisible }) => (
+                <Spring delay={100} to={{ opacity: isVisible ? 1 : 0 }}>
+                  {({ opacity }) => (
+                    <div className="bottom-description" style={{ ...empty, opacity }}>
+                      There&apos;s so much material, and it can be daunting. It might feel like you need a natural talent that
+                      you&apos;re born with...but let us prove you wrong. <br /> <strong>Learn music with us!</strong>
+                    </div>
+                  )}
+                </Spring>
+              )}
+            </VisbilitySensor>
+
           </div>
+
           {this.errorMessage()}
-          <div className="sign-up-section">
+          <div className="sign-up-section" ref={this.signupsection}>
             <h1 className="title" id="createAccount">Create account</h1>
-            <input className="emailSignup" onChange={this.onInputEmail} placeholder="email" />
-            <input className="usernameSignup" onChange={this.onInputUsername} placeholder="username" />
-            <input type="password" className="passwordSignup" onChange={this.onInputPassword} placeholder="password" />
+            <div className="signup-placeholder">
+              <input className="emailSignup" onChange={this.onInputEmail} placeholder="email" />
+              <input className="usernameSignup" onChange={this.onInputUsername} placeholder="username" />
+              <input type="password" className="passwordSignup" onChange={this.onInputPassword} placeholder="password" />
+            </div>
             <div className="input-subtitles">
               <div className="already-have">Already have an account?</div>
               <NavLink to="/signin">
@@ -116,6 +239,7 @@ class HomePageWithoutUser extends Component {
             </div>
             <button onClick={this.submit} type="submit" className="button" id="createaccountbutton">Create Account</button>
           </div>
+
         </div>
       </div>
     );
@@ -127,4 +251,5 @@ function mapStateToProps(reduxState) {
     authentication: reduxState.auth.authenticated,
   };
 }
+
 export default connect(mapStateToProps, { signupUser })(HomePageWithoutUser);
