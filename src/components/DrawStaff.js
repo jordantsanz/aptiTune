08132e8c8572801d5cac5ee46bfdf92c7ee179ee
Jacob Donoghue <jacob.l.donoghue.22@dartmouth.edu>
2020-08-28 20:51:17 -0700

    with easyscore support in rhythmsensor

--- src/components/DrawStaff.js
+++ src/components/DrawStaff.js
@@ -3,21 +3,23 @@ import Vex from 'vexflow';
 
 // IMPORTANT!! - define the id of the div where the staff will be drawn!
 const drawStaff = (notes, divId) => {
-  const vexnotes = notes.join(', '); // convert user's notes into string that can be rendered by vexflow
-  const vf = new Vex.Flow.Factory({
-    renderer: { elementId: divId, width: 500, height: 200 },
-  });
+  if (notes != null) {
+    const vexnotes = notes.join(', '); // convert user's notes into string that can be rendered by vexflow
+    const vf = new Vex.Flow.Factory({
+      renderer: { elementId: divId, width: 500, height: 200 },
+    });
 
-  const score = vf.EasyScore();
-  const system = vf.System();
+    const score = vf.EasyScore();
+    const system = vf.System();
 
-  system.addStave({
-    voices: [
-      score.voice(score.notes(vexnotes, { stem: 'up' })),
-    ],
-  }).addClef('treble').addTimeSignature('4/4'); // default is treble clef & 4/4 time, but we can change that and pass it in as a variable if needed!
-  console.log('drawing');
-  vf.draw();
+    system.addStave({
+      voices: [
+        score.voice(score.notes(vexnotes, { stem: 'up' })),
+      ],
+    }).addClef('treble').addTimeSignature('4/4'); // default is treble clef & 4/4 time, but we can change that and pass it in as a variable if needed!
+    console.log('drawing');
+    vf.draw();
+  }
 };
 
 export default drawStaff;
--- src/components/Lesson/Activities/RhythmSensor.js
+++ src/components/Lesson/Activities/RhythmSensor.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable max-len */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable jsx-a11y/control-has-associated-label */
@@ -11,6 +12,7 @@ import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 // import { SingleEntryPlugin } from 'webpack';
 import { getLesson } from '../../../actions/index';
+import drawStaff from '../../DrawStaff';
 
 function mapStateToProps(reduxState) {
   return {
@@ -38,6 +40,7 @@ class RhythmSensor extends Component {
       resultsReady: false,
       correct: false,
       firstAttempt: true,
+      scoreArray: [],
     };
   }
 
@@ -173,10 +176,13 @@ class RhythmSensor extends Component {
     }
 
     showProgress = () => {
-      const maxWidth = 300;
+      const maxWidth = 700;
       console.log('showing progress');
       let i = 0;
       const interval = this.calculateIntervalForProgress();
+      const partition = 0;
+      const incrementValue = this.calculateIncrementValue(maxWidth);
+      console.log('interval: ', interval);
       if (i === 0) {
         i = 1;
         const elem = document.getElementById('myBar');
@@ -186,68 +192,130 @@ class RhythmSensor extends Component {
             clearInterval(id);
             i = 0;
           } else {
-            width += maxWidth / (interval / 10);
+            width += incrementValue;
+            console.log('width incremented to width:', width);
             elem.style.width = `${width}%`;
           }
-        }, interval / 10);
+        }, interval);
       }
     }
 
     calculateIntervalForProgress = () => {
       const beatCount = this.state.page.activity.rhythmPattern.length - 1;
       const lastTime = parseInt(this.state.correctAnswers[beatCount], 10);
+      const firstTime = parseInt(this.state.correctAnswers[0], 10);
       console.log('BeatCount: ', beatCount);
       console.log('LastTime:', lastTime);
-      const interval = lastTime / beatCount;
+      const interval = (lastTime - firstTime) / beatCount;
       console.log('interval:', interval);
       return interval;
+    }
+
+    calculateIncrementValue = (maxWidth) => {
+      const beatCount = this.state.page.activity.rhythmPattern.length - 1;
+      const incrementValue = maxWidth / (beatCount);
+      console.log('incrementValue');
+      return incrementValue;
     }
 
     goToNext = () => {
       this.props.onSubmit();
     }
 
-    render() {
-      // add page for rendering
-      // console.log('Rendering with seedTime', this.state.seedTime);
-      // console.log('TIMES:', this.state.times);
+    createScoreArray = () => {
       const { pages } = this.props;
       const page = pages[this.state.pageNumber];
-      if (this.state.firstRender && page !== null && page !== undefined) {
+      let array = [];
+      page.activity.rhythmPattern.map((note) => {
+        if (note === '1') {
+          array = array.concat(['G4/w']);
+        } else if (note === '2') {
+          array = array.concat(['G4/h']);
+        } else if (note === '4') {
+          array = array.concat(['G4/q']);
+        } else if (note === '8') {
+          array = array.concat(['G4/8']);
+        } else if (note === '16') {
+          array = array.concat(['G4/8']);
+        }
+      });
+      console.log('scoreArray:', array);
+      this.setState({ scoreArray: array });
+      return array;
+    }
+
+    firstRender = () => {
+      const { pages } = this.props;
+      const page = pages[this.state.pageNumber];
+      if (page !== null && page !== undefined) {
         const { bpm } = page.activity;
         console.log('bpm:', bpm);
         const bps = bpm / 60;
         this.setState({ firstRender: false, bps, page: this.props.pages[this.state.pageNumber] });
+        this.createScoreArray();
       }
+    }
+
+    nullScoreArray = () => {
+      this.setState({ scoreArray: null });
+    }
+
+    render() {
       // console.log('page in listening', page);
       // console.log('correct answer:', page.activity.correct_answer);
+      if (this.state.firstRender) {
+        this.firstRender();
+      }
       if (this.state.page === null || this.state.page === undefined) {
         return (
-          <div>
+          <div className="rhythmActivity">
             Loading...
           </div>
         );
       }
-      console.log('rendering with correct', this.state.correct);
-      if (this.state.beginTapping && !this.state.resultsReady) {
+
+      if (this.state.scoreArray != null) {
+        const array = this.state.scoreArray;
+        this.nullScoreArray();
         return (
-          <div>
+          <div className="rhythmActivity">
+            <div>{drawStaff(this.state.scoreArray, 'rhythmScore')}</div>
+            <div>
+              <div>bps: {this.state.bps}</div>
+              <div>Seed time: {this.state.seedTime} </div>
+              <div>Clicked times: {this.state.times} </div>
+            </div>
             <div>{this.state.page.activity.rhythmPattern.map((n) => {
               return (
-                <div>1/{n} note</div>
+                <div id={n}>1/{n} note</div>
               );
             })}
             </div>
-            <button type="button" onClick={this.updateTime} style={{ color: this.state.buttonColor }}>{this.state.countDownNumber}</button>
+            <button type="button" onClick={this.updateTime}>Play</button>
+          </div>
+        );
+      }
+      if (this.state.beginTapping && !this.state.resultsReady) {
+        return (
+          <div>
             <div id="progress">
               <div id="myBar" />
+            </div>
+            <div className="rhythmActivity">
+              <div>{this.state.page.activity.rhythmPattern.map((n) => {
+                return (
+                  <div id={n}>1/{n} note</div>
+                );
+              })}
+              </div>
+              <button type="button" onClick={this.updateTime} style={{ color: this.state.buttonColor }}>{this.state.countDownNumber}</button>
             </div>
           </div>
         );
       }
       if (this.state.resultsReady && this.state.correct) {
         return (
-          <div>
+          <div className="rhythmActivity">
             Congrats! Time to move on
             <button type="button" className="nextButton" onClick={this.goToNext}>
               Next
@@ -257,11 +325,11 @@ class RhythmSensor extends Component {
       }
       if (!this.state.firstAttempt) {
         return (
-          <div>
+          <div className="rhythmActivity">
             <div> Not quite, try again!</div>
             <div>{this.state.page.activity.rhythmPattern.map((n) => {
               return (
-                <div>1/{n} note</div>
+                <div id={n}>1/{n} note</div>
               );
             })}
             </div>
@@ -269,8 +337,9 @@ class RhythmSensor extends Component {
           </div>
         );
       } else {
+        // console.log('rendering with score: ', score);
         return (
-          <div>
+          <div className="rhythmActivity">
             <div>
               <div>bps: {this.state.bps}</div>
               <div>Seed time: {this.state.seedTime} </div>
@@ -278,7 +347,7 @@ class RhythmSensor extends Component {
             </div>
             <div>{this.state.page.activity.rhythmPattern.map((n) => {
               return (
-                <div>1/{n} note</div>
+                <div id={n}>1/{n} note</div>
               );
             })}
             </div>
