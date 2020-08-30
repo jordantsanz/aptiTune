7e095ffddc335f7847a81f54f8ad7a42ed2bd923
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-08-30 03:04:00 -0500

    style

--- src/components/errorMessage.js
+++ src/components/errorMessage.js
@@ -3,16 +3,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { hideError } from '../actions';
 
+const ErrorMessage = (props) => {
+  return (
+    <div className="error-message-div">
+      {props.message}
+    </div>
+  );
+};
+
 class ErrorNotification extends Component {
   render() {
     if (this.props.error.open === true) {
-      if (this.props.error.message !== null) {
-        return (
-          <span role="button" tabIndex="0" className="error-message" onClick={() => { this.props.hideError(); }}>
-            {this.props.error.message}
-          </span>
-        );
-      }
+      const listErrors = this.props.error.messages.map((message) => (
+        <ErrorMessage className="error-message-div" message={message} key={message} />
+      ));
+
+      return (
+        <span role="button" tabIndex="0" className="error-message" onClick={() => { this.props.hideError(); }}>
+          {listErrors}
+        </span>
+      );
     }
     return null;
   }
